d934f4b49bd2832f2051350a86aca10cd83a6e1e
nathan <gon1914@naver.com>
2019-07-26 15:03:53 +0900

    i wrote some router code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import Homepage from './pages/homepages/homepage.component'
 import './App.css';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
