f603d4f301fd696427504df4096d382a57d832cd
nathan <gon1914@naver.com>
2019-08-05 16:56:27 +0900

    i made a type of action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -38,18 +38,40 @@ class App extends React.Component {
   }
 
   render() {
+
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin'
+            render={() => this.props.currentUser ? (
+              <Redirect to='/' />
+            ) : (<SignInAndSignUpPage />)}
+          />
         </Switch>
       </div>
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
@@ -57,4 +79,4 @@ const mapDispatchToProps = dispatch => ({
 
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
